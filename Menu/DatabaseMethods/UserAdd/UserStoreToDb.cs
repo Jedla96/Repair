b5727be1f256f6cdee61734177b9e0830152cd94
Jedla96 <a.jedlicka96@gmail.com>
2023-04-23 17:45:57 +0200

    Added second database for the next User setup

--- Menu/DatabaseMethods/UserAdd/UserStoreToDb.cs
+++ Menu/DatabaseMethods/UserAdd/UserStoreToDb.cs
@@ -13,17 +13,24 @@ public abstract class UserStoreToDb
             User user = users[index];
 
             {
-                string insertSql =
+                string insertSql1 =
                     $"INSERT INTO users (FirstName, LastName, DateOfBirth, Position, Code, Login, Password) VALUES (:FirstName, :LastName, :DateOfBirth, :Position, :Code, :Login, :Password)";
-                NpgsqlCommand insertCommand = new NpgsqlCommand(insertSql, connection);
-                insertCommand.Parameters.AddWithValue(":FirstName", user.FirstName!);
-                insertCommand.Parameters.AddWithValue(":LastName", user.LastName!);
-                insertCommand.Parameters.AddWithValue(":DateOfBirth", user.DateOfBirth);
-                insertCommand.Parameters.AddWithValue(":Position", user.Position!);
-                insertCommand.Parameters.AddWithValue(":Code", user.Code!);
-                insertCommand.Parameters.AddWithValue(":Login", user.Login!);
-                insertCommand.Parameters.AddWithValue(":Password", user.Password!);
-                insertCommand.ExecuteNonQuery();
+                NpgsqlCommand insertCommand1 = new NpgsqlCommand(insertSql1, connection);
+                insertCommand1.Parameters.AddWithValue(":FirstName", user.FirstName!);
+                insertCommand1.Parameters.AddWithValue(":LastName", user.LastName!);
+                insertCommand1.Parameters.AddWithValue(":DateOfBirth", user.DateOfBirth);
+                insertCommand1.Parameters.AddWithValue(":Position", user.Position!);
+                insertCommand1.Parameters.AddWithValue(":Code", user.Code!);
+                insertCommand1.Parameters.AddWithValue(":Login", user.Login!);
+                insertCommand1.Parameters.AddWithValue(":Password", user.Password!);
+                insertCommand1.ExecuteNonQuery();
+
+               string insertSql2 = $"INSERT INTO accounts (FirstName, LastName, Code) VALUES (:FirstName, :LastName, :Code)";
+                NpgsqlCommand insertCommand2 = new NpgsqlCommand(insertSql2, connection);
+                insertCommand2.Parameters.AddWithValue(":FirstName", user.FirstName!);
+                insertCommand2.Parameters.AddWithValue(":LastName", user.LastName!);
+                insertCommand2.Parameters.AddWithValue(":Code", user.Code!);
+                insertCommand2.ExecuteNonQuery();
             }
             connection.Close();
         }
